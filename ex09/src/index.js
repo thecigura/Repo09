287e52f51746f1bb1a0ab60b02fb98043f34350c
Denan <denan.cigura@gmail.com>
2021-09-22 09:58:24 +0200

    JS Ex 09

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+// Only change code below this line
+function functionElse(num) {
+    if (num <= 5) {
+        return "5 or smaller";
+    } else {
+    return "Bigger then 5";
+    }
+}
+result = functionElse(0);
+console.log(result);
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
+
+
+
+// Only change code above this line
+module.exports = functionElse;
